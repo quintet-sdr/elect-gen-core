45747dfe4769808f589f4bab70e073dbf079632c
lanebo1 <k.efimovich@innopolis.university>
2024-07-12 13:41:23 +0300

    perf: move from openpyxl lib to pandas

--- excel_util.py
+++ excel_util.py
@@ -2,8 +2,8 @@
 
 import os
 import random
-from openpyxl import load_workbook, Workbook
 from models import Course, Student
+import pandas as pd
 
 
 def readCoursesInfo():
@@ -12,13 +12,15 @@ def readCoursesInfo():
     """
 
     courseFile = 'Courses.xlsx'
-    courseWB = load_workbook(courseFile)
-    courseSheet = courseWB[courseWB.sheetnames[0]]
+
+    # Read the Excel file into a DataFrame
+    df = pd.read_excel(courseFile)
+
     courses = []
-    for i in range(2, courseSheet.max_row + 1):
-        ID = courseSheet.cell(row=i, column=1).value
-        name = courseSheet.cell(row=i, column=2).value
-        quota = courseSheet.cell(row=i, column=3).value
+    for _, row in df.iterrows():
+        ID = row[0]
+        name = row[1]
+        quota = row[2]
         courses.append(Course(ID, name, quota))
     return courses
 
@@ -31,17 +33,17 @@ def readStudentsInfo(courses, students, name):
     :return: None
     """
 
-    studentFile = name
-    studentWB = load_workbook(studentFile)
-    studentSheet = studentWB[studentWB.sheetnames[0]]
-    for i in range(2, studentSheet.max_row + 1):
-        ID = studentSheet.cell(row=i, column=1).value
-        name = studentSheet.cell(row=i, column=2).value
-        GPA = studentSheet.cell(row=i, column=3).value
+    # Read the Excel file into a DataFrame
+    df = pd.read_excel(name)
+
+    for _, row in df.iterrows():
+        ID = row[0]
+        name = row[1]
+        GPA = float(row[2])
         keywords = []
         availableCourses = []
-        for j in range(4, 9):
-            keyword = studentSheet.cell(row=i, column=j).value
+        for j in range(3, 8):
+            keyword = row[j]
             flag = False
             for course in courses:
                 if course.name.lower() == keyword.lower():
@@ -54,7 +56,6 @@ def readStudentsInfo(courses, students, name):
                 course = Course(-1, keyword, 0)
                 courses.append(course)
                 keywords.append(course)
-        GPA = float(GPA)
         if len(availableCourses) < 5:
             other_courses = [course.name for course in courses if course.name not in availableCourses]
             random_courses = random.sample(other_courses, 5 - len(availableCourses))
@@ -65,7 +66,7 @@ def readStudentsInfo(courses, students, name):
 
 
 def writeResults(students_distributions, costs, courses):
-    """Writes the results of the algorithm in the Results.xlsx file
+    """Writes the results of the algorithm in an Excel file
     :param students_distributions: list of lists of Student objects
     :param costs: list of costs
     :param courses: list of Course objects
@@ -75,37 +76,39 @@ def writeResults(students_distributions, costs, courses):
     print("Writing results in table...")
     if "Results.xlsx" in os.listdir():
         os.remove("Results.xlsx")
-    results = Workbook()
+    writer = pd.ExcelWriter('Results.xlsx', engine='xlsxwriter')
     costs_dict = {cost: students for students, cost in zip(students_distributions, costs)}
     numeration = 1
     for cost, students in costs_dict.items():
-        resultsSheetResults = results.create_sheet(title=f"Result {numeration}")
-        resultsSheetResults.append(
-            ["Student ID", "Student Name", "Final priority", "Course Name", "Actual Course Name", "1 priority",
-             "2 priority",
-             "3 priority",
-             "4 priority", "5 priority", "No priority", "Bad input", ""] + [course.name for course in courses])
-
         totalResults = [0] * 7
         totalCourseResults = [0] * len(courses)
         totalCourseQuotas = [course.quota for course in courses]
+        totalCourseNames = [course.name for course in courses]
         for student in students:
-            resultsSheetResults.append([student.ID, student.name, student.finalPriority, student.finalCourse,
-                                        student.keywords[
-                                            student.finalPriority - 1].name if student.finalPriority <= 5 else ""])
             if 1 <= student.finalPriority <= 7:
                 totalResults[student.finalPriority - 1] += 1
             matching_courses = [course for course in courses if course.name == student.finalCourse]
             if matching_courses:
                 totalCourseResults[courses.index(matching_courses[0])] += 1
-        for j in range(0, 7):
-            resultsSheetResults.cell(row=2, column=j + 6, value=totalResults[j])
-        resultsSheetResults.cell(row=2, column=13, value="Distribution:")
-        resultsSheetResults.cell(row=3, column=13, value="Quotas:")
-        for k in range(0, len(courses)):
-            resultsSheetResults.cell(row=2, column=k + 14, value=totalCourseResults[k])
-        for k in range(0, len(courses)):
-            resultsSheetResults.cell(row=3, column=k + 14, value=totalCourseQuotas[k])
+        max_length = max(len(totalResults), len(totalCourseResults), len(totalCourseQuotas), len(totalCourseNames))
+        totalResults.extend([0] * (max_length - len(totalResults)))
+        totalCourseResults.extend([0] * (max_length - len(totalCourseResults)))
+        totalCourseQuotas.extend([0] * (max_length - len(totalCourseQuotas)))
+        totalCourseNames.extend([""] * (max_length - len(totalCourseNames)))
+        statistics_data = {'Total Results': totalResults, 'Total Course Results': totalCourseResults,
+                           'Total Course Quotas': totalCourseQuotas, 'Total Course Names': totalCourseNames}
+        df_statistics = pd.DataFrame(statistics_data)
+        df_statistics.to_excel(writer, sheet_name=f'Result {numeration}', index=False)
+        data = {'Student ID': [student.ID for student in students],
+                'Student Name': [student.name for student in students],
+                'Final priority': [student.finalPriority for student in students],
+                'Course Name': [student.finalCourse for student in students],
+                'Actual Course Name': [
+                    student.keywords[student.finalPriority - 1].name if student.finalPriority <= 5 else "" for student
+                    in students]}
+        df = pd.DataFrame(data)
+        df.to_excel(writer, sheet_name=f'Result {numeration}', startrow=len(df_statistics) + 2, index=False)
         numeration += 1
+    writer._save()
+
     print("Results written in table")
-    results.save("Results.xlsx")
