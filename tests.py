ccbb43fa1433305dad0a00d0ff6c20b290deb804
Maksim Malov <malov-2005@bk.ru>
2024-07-17 17:14:47 +0300

    chore: update unittests

--- tests.py
+++ tests.py
@@ -1,12 +1,19 @@
+import copy
 import unittest
 import os
-from main import Student, Course, readCoursesInfo, readStudentsInfo, Distribute, costFunction, writeResults
+from main import Distribute, costFunction, startBasicAlgorithm, improveDistribution
+from models import Student, Course
+from excel_util import writeResults
+from json_util import readCoursesInfoJson, readStudentsInfoJson
 
 
 class TestStudentCourseAllocation(unittest.TestCase):
     def setUp(self):
-        self.courses = readCoursesInfo()
-        self.students = readStudentsInfo(self.courses)
+        self.courses_json_file = 'courses.json'
+        self.students_json_file = 'students.json'
+        self.courses = readCoursesInfoJson(self.courses_json_file)
+        self.students = []
+        readStudentsInfoJson(self.courses, self.students, self.students_json_file)
         self.errorCourse = Course(-1, "ERROR", 0)
 
     def test_readCoursesInfo(self):
@@ -17,18 +24,29 @@ class TestStudentCourseAllocation(unittest.TestCase):
         self.assertIsInstance(self.students, list)
         self.assertTrue(all(isinstance(student, Student) for student in self.students))
 
-    def test_writeResults(self):
-        writeResults(self.students)
-        self.assertTrue(os.path.exists('Results.xlsx'))
+    def test_student_finalPriority(self):
+        startBasicAlgorithm(self.students_json_file, self.courses_json_file)
+        self.assertTrue(all(1 <= student.finalPriority <= 7 for student in self.students))
 
     def test_costFunction(self):
-        Distribute(self.students, self.errorCourse)
-        cost = costFunction(self.students)
+        startBasicAlgorithm(self.students_json_file, self.courses_json_file)
+        cost = costFunction(self.students, self.courses)
         self.assertIsInstance(cost, float)
 
-    def test_student_finalPriority(self):
-        Distribute(self.students, self.errorCourse)
-        self.assertTrue(all(1 <= student.finalPriority <= 7 for student in self.students))
+    def test_improveDistribution(self):
+        students_copy = [copy.deepcopy(s) for s in self.students]
+        courses_copy = [copy.deepcopy(c) for c in self.courses]
+        students_copy, initial_cost = Distribute(students_copy, courses_copy)
+        improved_students, improved_cost = improveDistribution(students_copy, courses_copy)
+        self.assertLessEqual(improved_cost, initial_cost)
+        self.assertTrue(all(student.isDistributed for student in improved_students))
+        self.assertIsInstance(improved_cost, float)
+
+    def test_writeResults(self):
+        best_distributions, best_distribution_costs, courses_rate_dict = startBasicAlgorithm(self.students_json_file,
+                                                                                             self.courses_json_file)
+        writeResults(best_distributions, best_distribution_costs, self.courses, courses_rate_dict)
+        self.assertTrue(os.path.exists('Results.xlsx'))
 
 
 if __name__ == '__main__':
